abb56c85df26eb94f19e292e0a1dcc1826f4131d
Pranish Shrestha <shresthapranish99@gmail.com>
2019-11-10 00:44:37 +1100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Pokemon from './components/pokemon';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+class App extends Component{
+
+  state = {
+    pokemon_name: undefined,
+    pokemon_type: undefined,
+    pokemon_image: undefined
+
+  }
+
+   getPokemon = async (e) =>{
+     e.preventDefault();
+    const id = e.target.elements.pokemonid.value;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    console.log(data);
+   this.setState({
+     pokemon_name: data.name.toUpperCase(),
+     pokemon_type:  data.types[0].type.name.toUpperCase(),
+     pokemon_image:data.sprites.back_default
+   })
+
+  }
+    render(){
+    return (
+      <div>
+      <Pokemon getPokemon={this.getPokemon}
+      pokemon_name={this.state.pokemon_name}
+      pokemon_type={this.state.pokemon_type}
+      pokemon_image={this.state.pokemon_image}/>
+      </div>
+    );
+
+    }
+ }
 export default App;
